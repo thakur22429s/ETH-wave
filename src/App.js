614a2c2991b76c578189c4281a48cfe446ac7c3d
Abhay Singh Thakur <thakur12@purdue.edu>
2022-11-02 03:37:15 -0400

    Added getAllWaves function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,52 @@ import abi from './utils/WavePortal.json';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
   /**
    * Create a varaible here that holds the contract address after you deploy!
    */
   const contractAddress = "0x954839CbBf95C5a4b7F9b1648Cd5Ff0dEE2c00f2";
   const contractABI = abi.abi;
-  
+
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -27,7 +67,8 @@ const App = () => {
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found")
       }
@@ -48,7 +89,7 @@ const App = () => {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
     }
@@ -65,7 +106,7 @@ const App = () => {
 
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
-        
+
         const waveTxn = await wavePortalContract.wave();
         console.log("Mining...", waveTxn.hash);
 
@@ -85,19 +126,19 @@ const App = () => {
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
-  
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-        I am Abhay. 
-        This is my personal social wall built on the blockchain and secured by smart contracts. 
-        Feel free to drop a message, a link to your favorite Spotify playlist, 
-        or anything that you want to share with the world!
+          I am Abhay.
+          This is my personal social wall built on the blockchain and secured by smart contracts.
+          Feel free to drop a message, a link to your favorite Spotify playlist,
+          or anything that you want to share with the world!
         </div>
 
         <button className="waveButton" onClick={wave}>
@@ -109,6 +150,15 @@ const App = () => {
             Connect Wallet
           </button>
         )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
